95d971e5c1bed39014ec87a5541315987c758e36
unknown <vishpatel528@gmail.com>
2022-03-30 19:19:55 +0530

    Task6: Added

--- Assets/Scripts/testing.cs
+++ Assets/Scripts/testing.cs
@@ -1,27 +1,5 @@
-using System.Collections;
 using UnityEngine;
 
 public class testing : MonoBehaviour
 {
-    delegate IEnumerator myDel();
-    myDel del;
-    
-    void OnEnable()
-    {
-        del += MyCoroutine;
-    }
-    void Update()
-    {
-        if(Input.anyKey)
-            StartCoroutine(del());
-    }
-    IEnumerator MyCoroutine()
-    {
-        yield return null;
-        transform.Rotate(Vector3.up);
-    }
-    void OnDisable()
-    {
-        del -= MyCoroutine;
-    }
 }
