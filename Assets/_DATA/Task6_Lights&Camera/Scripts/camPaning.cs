08bf227ff00ff4e04629afdfe37cfadc4df51265
unknown <vishpatel528@gmail.com>
2022-03-31 16:29:06 +0530

    update

--- Assets/_DATA/Task6_Lights&Camera/Scripts/camPaning.cs
+++ Assets/_DATA/Task6_Lights&Camera/Scripts/camPaning.cs
@@ -3,7 +3,6 @@ public class camPaning : MonoBehaviour
 {
     Vector3 rotation;
     public float speed = 3;
-
     void Update()
     {
         if (Input.GetMouseButton(0))
@@ -11,6 +10,7 @@ public class camPaning : MonoBehaviour
             rotation.y += Input.GetAxis("Mouse X");
             rotation.x += -(Input.GetAxis("Mouse Y"));
             transform.eulerAngles = rotation * speed;
+            //transform.Rotate();
         }
     }
 }
